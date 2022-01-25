b210d3bf9a033765d356b9b234a63a94758a221d
Nils Fürniß <contact@nalsai.de>
2022-01-25 17:55:38 +0100

    implemented input and output of text
    (also made some placeholder functions)

--- src/main.py
+++ src/main.py
@@ -1,30 +1,37 @@
 #!/usr/bin/env python3
 
-"""A Program to summarize text, I guess."""
+"""A program to summarize text, I guess."""
 
 import argparse
-
+import sys
+import text_summarize
 
 def parse_command_line():
     """Function to parse user input."""
-    parser = argparse.ArgumentParser(description="""Program to summarize text.
-    Please provide an input file.""")
-    parser.add_argument("-i", "--input", help="path of input text file.")
-    parser.add_argument("-o", "--output", help="path of output text file.")
+    parser = argparse.ArgumentParser(description="""A program to summarize text.
+    Please provide an input file or text.""")
+    parser.add_argument("-i", "--input", help="path of input text file (use - to read from stdin)")
+    parser.add_argument("-o", "--output", help="path of output text file (prints to stdout if not specified)")
+    parser.add_argument("-n", "--number", help="number of sentences")
 
     args = parser.parse_args()
 
     input_file = args.input
     output_file = args.output
-    return input_file, output_file
+    sentences_number = args.number
+    return input_file, output_file, sentences_number
 
 if __name__ == "__main__":
     try:
-        input_f, output_f = parse_command_line()
+        input_f, output_f, sentences_n = parse_command_line()
 
-        with open(input_f) as file:
-            text = file.read()
-            print(text)
+        if input_f == "-":
+            text_input = sys.stdin.read()
+            print(text_input)
+        else:
+            with open(input_f, "r") as file:
+                text_input = file.read()
+                print(text_input)
 
     except FileNotFoundError:
         print("An error occured.")
@@ -33,3 +40,11 @@ if __name__ == "__main__":
         print("An error occured.")
         print("Did you specify an input path?")
         print("Check -h or --help for options.")
+
+    if text_input:
+        summarized_text = text_summarize.do_stuff(text_input, sentences_n)
+        if output_f:
+            with open(output_f, "w") as file:
+                file.write(summarized_text)
+        else:
+            print(summarized_text)
---
+++ src/text_summarize.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+"""idk"""
+
+def do_stuff(input_text, number_of_sentences):
+    """Does stuff, but not yet"""
+    return input_text
+
+def clean(input_text):
+    """Cleans the text"""
+    return input_text
+
+def dissect(input_text):
+    """Dissects the text into its sentences"""
+    pass
+
+def remove_stop_words():
+    """Removes stop words"""
+    pass
+
+def calculate_histogram():
+    """Calculates word histogram"""
+    pass
+
+def sort_importance():
+    """Sorts the sentences by \"importance\""""
+    pass
